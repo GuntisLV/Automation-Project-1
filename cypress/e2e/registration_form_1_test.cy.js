28c5c7952ec55a3a86da37aa7fe17f8c6b416e0c
GuntisLV <pastorsguntis@inbox.lv>
2023-06-16 22:53:52 +0300

    Create New Tests for Form 1

--- cypress/e2e/registration_form_1_test.cy.js
+++ cypress/e2e/registration_form_1_test.cy.js
@@ -16,12 +16,21 @@ Assignment 2:
  5. Add comment to the first test containing today’s date
  */
 
-describe('This is first test suite', () => {
+describe('This is first test suite, Guntis Pastors', () => {
+    // 16.06.2023
     it('User can submit data only when valid mandatory values are added', () => {
+
+        // class="input" => .input
+        // id="test" => #test
+        // data-testid="test2" => [data-testid="lastNameTestId"]
+    
+
+        cy.get('[data-testid="phoneNumberTestId"]').type('555666777')
+        cy.get('#firstName').type('Guntis')
+        cy.get('#lastName').type('Pastors')
+        cy.get('input[name="password"]').type('1234')
+        cy.get('[name="confirm"]').type('1234')
         cy.get('#username').type('Something')
-        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
-        cy.get('input[name="password"]').type('MyPass')
-        cy.get('[name="confirm"]').type('MyPass')
 
         //in order to activate submit button, user has to click somewhere outside the input field
         cy.get('h2').contains('Password').click()
@@ -97,18 +106,95 @@ describe('This is first test suite', () => {
         // Add test, similar to previous one with phone number field not filled in
         // All other fields should be entered correctly
         // Assert that submit button is not enabled and that successful message is not visible
+        cy.get('#username').type('Guntis')
+        cy.get('input[name="password"]').type('MyPass')
+        cy.get('[name="confirm"]').type('MyPass')
+
+        // Scroll back to username input field
+        cy.get('#username').scrollIntoView()
+        cy.get('#username').clear().type('  ')
+        cy.get('h2').contains('Password').click()
+
+        // Asserting that Submit button is disabled
+        cy.get('.submit_button').should('be.disabled')
+
+        // Assert that success message is not visible
+        cy.get('#success_message').should('not.be.visible')
+
+        // Assert that correct error message is visible and contain Mandatory input field...
+        cy.get('#input_error_message').should('be.visible').should('contain', 'Mandatory input field is not valid or empty!')
+
+        // Assert that username has tooltip with error message
+        cy.get('input[name="username"]').should('have.attr', 'title').should('contain', 'Input field')
+
+        // There are 2 options how to check error message visibility: using CSS or simply be.visible
+        // none = not visible; block = visible
+        cy.get('#input_error_message').should('be.visible')
+        cy.get('#input_error_message').should('have.css', 'display', 'block')
     })
 
     it('User cannot submit data when password and/or confirmation password is absent', () => {
         // Add test, similar to previous one with password field not filled in
         // All other fields should be entered correctly
         // Assert that submit button is not enabled and that successful message is not visible
+        cy.get('#username').type('Guntis')
+        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
+       
+
+        // Scroll back to username input field
+        cy.get('#username').scrollIntoView()
+        cy.get('#username').clear().type('  ')
+        cy.get('h2').contains('Password').click()
+
+        // Asserting that Submit button is disabled
+        cy.get('.submit_button').should('be.disabled')
+
+        // Assert that success message is not visible
+        cy.get('#success_message').should('not.be.visible')
+
+        // Assert that correct error message is visible and contain Mandatory input field...
+        cy.get('#input_error_message').should('be.visible').should('contain', 'Mandatory input field is not valid or empty!')
+
+        // Assert that username has tooltip with error message
+        cy.get('input[name="username"]').should('have.attr', 'title').should('contain', 'Input field')
+
+        // There are 2 options how to check error message visibility: using CSS or simply be.visible
+        // none = not visible; block = visible
+        cy.get('#input_error_message').should('be.visible')
+        cy.get('#input_error_message').should('have.css', 'display', 'block')
     })
 
     it('User cannot add letters to phone number', () => {
         // Next verification is given as example
         // how we can check from html code, that phone number should contain only numbers
         cy.get('[data-testid="phoneNumberTestId"]').should('have.attr', 'type', 'number')
+        cy.get('#username').type('Guntis')
+        cy.get('[data-testid="phoneNumberTestId"]').type('Cerebrum Hub')
+        cy.get('input[name="password"]').type('MyPass')
+        cy.get('[name="confirm"]').type('MyPass')
+       
+
+        // Scroll back to username input field
+        cy.get('#username').scrollIntoView()
+        cy.get('#username').clear().type('  ')
+        cy.get('h2').contains('Password').click()
+
+        // Asserting that Submit button is disabled
+        cy.get('.submit_button').should('be.disabled')
+
+        // Assert that success message is not visible
+        cy.get('#success_message').should('not.be.visible')
+
+        // Assert that correct error message is visible and contain Mandatory input field...
+        cy.get('#input_error_message').should('be.visible').should('contain', 'Mandatory input field is not valid or empty!')
+
+        // Assert that username has tooltip with error message
+        cy.get('input[name="username"]').should('have.attr', 'title').should('contain', 'Input field')
+
+        // There are 2 options how to check error message visibility: using CSS or simply be.visible
+        // none = not visible; block = visible
+        cy.get('#input_error_message').should('be.visible')
+        cy.get('#input_error_message').should('have.css', 'display', 'block')
 
         // Add steps, when all fields are correctly filled in, except phone number
         // Try typing letters to phone number field
